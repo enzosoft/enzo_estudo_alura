008c00c73c6b70a2e6cf4577924c67763ae346ea
enzosoft <enzo.leandrini@sou.unifeob.edu.br>
2024-12-11 13:32:19 -0300

    aula 1

---
+++ type-null-undefined.js
@@ -0,0 +1,17 @@
+// NULL
+
+let input = null;
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
+
+// EXEMPLO DE NULL - UNDEFINED
+
+let input1 = null;
+let input2;
+
+console.log(input1); // null
+console.log(input2); // undefined
---
+++ variaveis.js
@@ -0,0 +1,36 @@
+// VAR 
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area)
+// var area;
+
+// LET
+
+// let forma = 'retângulo';
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if (forma === 'retângulo'){
+//     area = altura * comprimento;
+// } else {
+//     area = (altura * comprimento) / 2;
+// }
+// console.log(area)
+
+// CONST
+
+const forma = 'triãngulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if(forma === 'quadrado'){
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+console.log(area)
